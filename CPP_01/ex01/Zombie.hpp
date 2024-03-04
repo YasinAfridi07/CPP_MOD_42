c6be1c21ca818405766349e51f76a6ba9851fe14
Yasin Usman <119350611+YasinAfridi07@users.noreply.github.com>
2024-03-04 09:02:29 +0000

    cpp01 ex01 class

---
+++ CPP_01/ex01/Zombie.hpp
@@ -0,0 +1,19 @@
+
+
+#ifndef "ZOMBIE_HPP"
+#define "ZOMBIE_HPP"
+
+#include <iostream>
+
+class ZOMBIE_HPP{
+    privete:
+        std::string _name;
+    public:
+        Zombie();
+        ~Zombie();
+
+        void annonce(void);
+}
+
+
+#endif
