dcfb57d039317032ca959569f85fd23061ca1f7b
Yasin Usman <yusman@lab1r6s2.42abudhabi.ae>
2024-03-07 15:47:24 +0400

    some changes

--- CPP_01/ex02/main.cpp
+++ CPP_01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yusman <yusman@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 18:52:02 by yusman            #+#    #+#             */
-/*   Updated: 2024/03/06 19:15:00 by yusman           ###   ########.fr       */
+/*   Updated: 2024/03/07 15:46:04 by yusman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,16 @@
 
 int main()
 {
-	std::string str = "HI THIS IS BRAIN";
+	std::string str =  "HI THIS IS BRAIN";
 	std::string *stringPTR = &str;
 	std::string &stringREF = str;
 
-	std::cout << &str << std::endl; // print memory address
-	std::cout << stringPTR << std::endl; // memory address of string PTR
-	std::cout << &stringREF << std::endl; // memory address of string REF
+	std::cout << &str << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
 
-	std::cout << str << std::endl; // value of str
-	std::cout << *stringPTR << std::endl; // value pointed to by stringPTR
-	std::cout << stringREF << std::endl; // value pointed to by stringREF
+	std::cout << str << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+
 }
