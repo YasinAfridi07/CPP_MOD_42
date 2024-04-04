1beb98d229ac6c75a01c81fa6f6bdb0b91d2534f
Yasin Usman <yusman@lab1r4s3.42abudhabi.ae>
2024-04-05 01:19:44 +0400

    ex00

--- CPP_04/ex00/Animal.cpp
+++ CPP_04/ex00/Animal.cpp
@@ -6,7 +6,7 @@
 /*   By: yusman <yusman@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 02:46:16 by yusman            #+#    #+#             */
-/*   Updated: 2024/04/03 06:27:18 by yusman           ###   ########.fr       */
+/*   Updated: 2024/04/05 00:17:19 by yusman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,18 @@ Animal::Animal(): _type("Animel")
 	std::cout << this->_type << " Default Constructor Called" << std::endl;
 }
 
-//td::cout << "\033[1;31m [ScavTrap] Destructor Called \033[0m" << std::endl;
 
 Animal::Animal(std::string type): _type(type)
 {
-	std::cout << "Animal " << this->_type << " Constructor Called" << std::endl;
-}// why 2 Constructor
+	std::cout << "\033[1;31m[Animal] "  << this->_type << " Constructor Called \033[0m" << std::endl;
+}
 
 Animal::~Animal()
 {
 	std::cout << "Animal Destructor Called" << std::endl;
 }
+
+
 
 Animal::Animal( const Animal &object )
 {
--- CPP_04/ex00/Cat.cpp
+++ CPP_04/ex00/Cat.cpp
@@ -6,16 +6,16 @@
 /*   By: yusman <yusman@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 02:46:11 by yusman            #+#    #+#             */
-/*   Updated: 2024/04/03 06:23:52 by yusman           ###   ########.fr       */
+/*   Updated: 2024/04/04 22:37:28 by yusman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "Cat.hpp"
 
-Cat::Cat() : Animal("cat")
+Cat::Cat() : Animal("Cat")
 {
-	std::cout  << "[CAT] Default Constructor Called" << std::endl;
+	std::cout  << "\033[1;31m[CAT] Default Constructor Called \033[0m" << std::endl;
 }
 
 Cat::~Cat()
